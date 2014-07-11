e3146611293ff378ab1391e0bcbe3662df9f47f4
Ashley Bellas <abellas@eos.ubc.ca>
2014-07-11 11:42:09 -0700

    Removed ThPa2D.TVD

--- prelim-model/ThPa2D.py
+++ prelim-model/ThPa2D.py
@@ -243,60 +243,6 @@ def flux(g, h, t, T, u, k_ad, k_de, Q, S, dt):
                 h.a[:] = bnew[:]
                 t += dt
         return g, h
-    
-    
-    
-def TVD(g, h, t, T, u, k_ad, k_de, Q, S, dt):
-
-	# extract the velocities
-	uz = u[:, :, 0]
-	ux = u[:, :, 1]
-
-	# evolution loop
-	anew = g.a
-	bnew = h.a
-
-	# define upwind for x, z OUTSIDE loop ONLY while du/dt = 0
-	p_upx = np.sign(ux)*0.5*( np.sign(ux) - 1)
-	n_upx = np.sign(ux)*0.5*( np.sign(ux) + 1)
-	p_upz = np.sign(uz + S)*0.5*( np.sign(uz + S) - 1)
-	n_upz = np.sign(uz + S)*0.5*( np.sign(uz + S) + 1)
-
-	while (t < T):
-
-		# fill the boundary conditions
-		g.fillBCs()
-		h.fillBCs()
-
-		# loop over zones: note since we are periodic and both endpoints
-		# are on the computational domain boundary, we don't have to
-		# update both g.ilo and g.ihi -- we could set them equal instead.
-		# But this is more general
-
-
-                i = np.arange(g.ilo + 1, g.ihi, 1, dtype = int)
-                j = np.arange(g.jlo + 1, g.jhi, 1, dtype = int)
-                [i , j] = np.meshgrid(i,j)
-                # upwind numerical solution
-
-                # dissolved:
-                anew[i, j] = g.a[i, j] + ( Q - k_ad[i, j] * g.a[i, j] + k_de[i, j] * h.a[i, j] +
-                                          
-                    ux[i, j] * ( n_upx[i, j]*(g.a[i, j - 1] - g.a[i, j + 1]) + p_upx[i, j]*(g.a[i, j + 1] - g.a[i, j - 1]) ) / g.dx + 
-                    uz[i, j] * ( n_upz[i, j]*(g.a[i - 1, j] - g.a[i + 1, j]) + p_upz[i, j]*(g.a[i + 1, j] - g.a[i - 1, j]) ) / g.dz ) * dt
-
-                # particulate:
-                bnew[i, j] = h.a[i, j] + ( S * ( n_upz[i, j]*h.a[i - 1, j] - h.a[i, j] + p_upz[i, j]*h.a[i + 1, j]) / h.dz + 
-                          k_ad[i, j] * g.a[i, j] - k_de[i, j] * h.a[i, j] + 
-                    ux[i, j] * ( n_upx[i, j]*(h.a[i, j - 1] - h.a[i, j + 1]) + p_upx[i, j]*(h.a[i, j + 1] - h.a[i, j - 1] ) ) / h.dx +
-                    uz[i, j] * ( n_upz[i, j]*(h.a[i - 1, j] - h.a[i + 1, j]) + p_upz[i, j]*(h.a[i + 1, j] - h.a[i - 1, j] ) ) / h.dz ) * dt
-
-
-                # store the (time) updated solution
-                g.a[:] = anew[:]
-                h.a[:] = bnew[:]
-                t += dt
-        return g, h
 
 
 def u_zero(nz, nx):
