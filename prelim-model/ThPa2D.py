Fixed indexing error in flux_cen calculation of TVD code

prelim-model/ThPa2D.py
@@ -183,15 +183,18 @@ def TVD(conc, u, p_upz, n_upz, p_upx, n_upx, sinkrate):
         fluxx_up = np.zeros((nz, nx));         fluxz_up = np.zeros((nz, nx))
         fluxx_up[:, 1:nx-2] = ux[:, 0:nx-3]*conc.a[:, 0:nx-3] * n_upx[:, 0:nx-3] + ux[:, 2:nx-1]*conc.a[:, 2:nx-1] * p_upx[:, 1:nx-2]
         fluxz_up[1:nz-2, :] = uz[0:nz-3, :]*conc.a[0:nz-3, :] * n_upz[0:nz-3, :] + uz[2:nz-1, :]*conc.a[2:nz-1, :] * p_upz[1:nz-2, :]
-
-        # d(conc)/dt based on upstream
-        tau_up = np.zeros((nz, nx))
-        tau_up[1:nz-2, 1:nx-2] = ((fluxx_up[1:nz-2, 0:nx-3] - fluxx_up[1:nz-2, 1:nx-2]) * n_upx[1:nz-2, 1:nx-2] + (fluxx_up[1:nz-2, 1:nx-2] - fluxx_up[1:nz-2, 2:nx - 1]) * p_upx[1:nz-2, 1:nx-2]) * conc.dx_i + ((fluxz_up[0:nz-3, 1:nx-2] - fluxz_up[1:nz-2, 1:nx-2]) * n_upz[1:nz-2, 1:nx-2] + (fluxz_up[1:nz-2, 1:nx-2] - fluxz_up[2:nz - 1, 1:nx-2]) * p_upz[1:nz-2, 1:nx-2]) * conc.dz_i 
+        
+        # d(conc)/dt according to upstream scheme
+        dtau_up_dt = np.zeros((nz, nx))
+        dtau_up_dt[1:nz-2, 1:nx-2] = ((fluxx_up[1:nz-2, 0:nx-3] - fluxx_up[1:nz-2, 1:nx-2]) * n_upx[1:nz-2, 0:nx-3] + (fluxx_up[1:nz-2, 1:nx-2] - fluxx_up[1:nz-2, 2:nx-1]) * p_upx[1:nz-2, 1:nx-2]) * conc.dx_i + ((fluxz_up[0:nz-3, 1:nx-2] - fluxz_up[1:nz-2, 1:nx-2]) * n_upz[0:nz-3, 1:nx-2] + (fluxz_up[1:nz-2, 1:nx-2] - fluxz_up[2:nz-1, 1:nx-2]) * p_upz[1:nz-2, 1:nx-2]) * conc.dz_i 
+        
+        # new concentration based on upstream scheme
+        tau_up = conc.a + dtau_up_dt * dt
 
         # define centred flux
         fluxx_cen = np.zeros((nz, nx));         fluxz_cen = np.zeros((nz, nx))
-        fluxx_cen[:, 1:nx-2] = 0.5 * ( conc.a[:, 0:nx-3]*ux[:, 0:nx-3] - conc.a[:, 2:nx-1]*ux[:, 2:nx-1] ) 
-        fluxz_cen[1:nz-2, :] = 0.5 * ( conc.a[0:nz-3, :]*uz[0:nz-3, :] - conc.a[2:nz-1, :]*uz[2:nz-1, :] )
+        fluxx_cen[:, 0:nx-1] = 0.5 * ( conc.a[:, 0:nx-1]*ux[:, 0:nx-1] - conc.a[:, 1:nx]*ux[:, 1:nx] ) 
+        fluxz_cen[0:nz-1, :] = 0.5 * ( conc.a[0:nz-1, :]*uz[0:nz-1, :] - conc.a[1:nz, :]*uz[1:nz, :] )
 
         # define anti-diffusive flux; shape =  [nz, nx]
         adfx = fluxx_cen - fluxx_up
@@ -208,13 +211,14 @@ def TVD(conc, u, p_upz, n_upz, p_upx, n_upx, sinkrate):
             zup[i, 1:nx-1] = max( np.max(conc.a[i-1:i+1, 1:nx-1]), np.max(tau_up[i-1:i+1, 1:nx-1]) )    # C
             zdo[i, 1:nx-1] = min( np.min(conc.a[i-1:i+1, 1:nx-1]), np.min(tau_up[i-1:i+1, 1:nx-1]) )    # C
 
-        # define pos/neg fluxes in x  
+        # define influx and outflux in x
         xpos = np.zeros((nz, nx)); xneg = np.zeros((nz, nx))  
         nfluxx = np.sign(adfx)*0.5*(np.sign(adfx) - 1)
         pfluxx = np.sign(adfx)*0.5*(np.sign(adfx) + 1)
         xpos[0:nz - 1, 1:nx - 1] = pfluxx[0:nz - 1, 0:nx - 2] * adfx[0:nz - 1, 0:nx - 2] - nfluxx[0:nz - 1, 1:nx - 1] * adfx[0:nz - 1, 1:nx - 1]
         xneg[0:nz - 1, 1:nx - 1] = pfluxx[0:nz - 1, 1:nx - 1] * adfx[0:nz - 1, 1:nx - 1] - nfluxx[0:nz - 1, 0:nx - 2] * adfx[0:nz - 1, 0:nx - 2]
-        # define pos/neg fluxes in z 
+        
+        # define influx and outflux in z 
         zpos = np.zeros((nz, nx)); zneg = np.zeros((nz, nx))
         nfluxz = np.sign(adfz)*0.5*(np.sign(adfz) - 1)
         pfluxz = np.sign(adfz)*0.5*(np.sign(adfz) + 1)
@@ -222,10 +226,10 @@ def TVD(conc, u, p_upz, n_upz, p_upx, n_upx, sinkrate):
         zneg[1:nz - 1, 0:nx - 1] = pfluxz[1:nz - 1, 0:nx - 1] * adfz[1:nz - 1, 0:nx - 1] - nfluxz[0:nz - 2, 0:nx - 1] * adfz[0:nz - 2, 0:nx - 1]
 
         # calculate the Zalesak parameter
-        zbetaup = (zup - tau_up) / zpos #* conc.dx/dt                                                 # C / (C m/s) * m/s = non dimensional
-        zbetado = (tau_up - zdo) / zneg #* conc.dx/dt
-        xbetaup = (xup - tau_up) / xpos #* conc.dz/dt
-        xbetado = (tau_up - xdo) / xneg #* conc.dz/dt
+        zbetaup = (zup - tau_up) / zpos# * conc.dx/dt                                                 # C / (C m/s) * m/s = non dimensional
+        zbetado = (tau_up - zdo) / zneg# * conc.dx/dt
+        xbetaup = (xup - tau_up) / xpos# * conc.dz/dt
+        xbetado = (tau_up - xdo) / xneg# * conc.dz/dt
         # remove nans and infs
         zeros = np.zeros(np.shape(zbetaup))
         idx = np.isnan(zbetaup)
@@ -278,7 +282,7 @@ def TVD(conc, u, p_upz, n_upz, p_upx, n_upx, sinkrate):
 
         # final sol.
         adv = np.zeros((nz, nx))
-        adv[1:nz-1, 1:nx-1] = tau_up[1:nz-1, 1:nx-1] +  (aax[1:nz-1, 0:nx-2] - aax[1:nz-1, 1:nx-1]) * conc.dx_i + (aaz[0:nz-2, 1:nx-1] - aaz[1:nz-1, 1:nx-1]) * conc.dz_i 
+        adv[1:nz-1, 1:nx-1] = dtau_up_dt[1:nz-1, 1:nx-1] +  (aax[1:nz-1, 0:nx-2] - aax[1:nz-1, 1:nx-1]) * conc.dx_i + (aaz[0:nz-2, 1:nx-1] - aaz[1:nz-1, 1:nx-1]) * conc.dz_i 
 
                
         return adv